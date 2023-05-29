95f541e78096043724114de107e0a0c39417092f
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-05-29 19:07:23 +0200

    654

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './ImageGalleryItem.module.css';
 
-const ImageGalleryItem = ({ src, alt, id, onOpenModal, largeImageURL }) => {
+const ImageGalleryItem = ({ src, alt, onOpenModal, largeImageURL }) => {
   return (
     <>
       <li
